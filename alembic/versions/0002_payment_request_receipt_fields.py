ac42fd9738bf457e09a6fa25e0a6f319b360372d
PsychicBAM <abdallahbahi31@gmail.com>
2026-06-09 01:11:59 +0300

    Shorten Alembic revision id

--- alembic/versions/0002_payment_request_receipt_fields.py
+++ alembic/versions/0002_payment_request_receipt_fields.py
@@ -1,6 +1,6 @@
 """Add payment request receipt and amount fields.
 
-Revision ID: 0002_payment_request_receipt_fields
+Revision ID: 0002_receipt_fields
 Revises: 0001_initial_schema
 Create Date: 2026-06-08
 """
@@ -12,7 +12,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "0002_payment_request_receipt_fields"
+revision: str = "0002_receipt_fields"
 down_revision: Union[str, None] = "0001_initial_schema"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- alembic/versions/0003_payment_request_provisioning_status.py
+++ alembic/versions/0003_payment_request_provisioning_status.py
@@ -1,7 +1,7 @@
 """Add provisioning status fields to payment_requests.
 
-Revision ID: 0003_payment_request_provisioning_status
-Revises: 0002_payment_request_receipt_fields
+Revision ID: 0003_provisioning_status
+Revises: 0002_receipt_fields
 Create Date: 2026-06-08
 """
 
@@ -12,8 +12,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "0003_payment_request_provisioning_status"
-down_revision: Union[str, None] = "0002_payment_request_receipt_fields"
+revision: str = "0003_provisioning_status"
+down_revision: Union[str, None] = "0002_receipt_fields"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
